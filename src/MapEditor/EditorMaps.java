02f4e32c405ff105578ad4d438431d420f30a7ed
Humna Hanif <hhanif@quinnipiac.edu>
2021-11-12 12:58:35 -0500

    level 5 started

--- src/MapEditor/EditorMaps.java
+++ src/MapEditor/EditorMaps.java
@@ -6,6 +6,7 @@ import Maps.TitleScreenMap;
 import Maps.TestMap2;
 import Maps.TestMap3;
 import Maps.TestMap4;
+import Maps.TestMap5;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,7 @@ public class EditorMaps {
             add("TestMap2");
             add("TestMap3");
             add("TestMap4");
+            add("TestMap5");
             add("TitleScreen");
         }};
     }
@@ -30,6 +32,8 @@ public class EditorMaps {
                 return new TestMap3();
             case "TestMap4":
             	return new TestMap4();
+            case "TestMap5":
+            	return new TestMap5();
             case "TitleScreen":
                 return new TitleScreenMap();
             default:
---
+++ src/Maps/TestMap5.java
@@ -0,0 +1,78 @@
+package Maps;
+
+import Enemies.BugEnemy;
+import Enemies.DinosaurEnemy;
+import Engine.ImageLoader;
+import EnhancedMapTiles.EndLevelBox;
+import EnhancedMapTiles.HorizontalMovingPlatform;
+import GameObject.Rectangle;
+import Level.*;
+import NPCs.Walrus;
+import Tilesets.CommonTileset;
+import Utils.Direction;
+import Utils.Point;
+
+import java.util.ArrayList;
+
+// Represents a test map to be used in a level
+public class TestMap5 extends Map {
+    private BugEnemy SecondBug, ThirdBug;
+
+    public TestMap5() {
+        super("test_map5.txt", new CommonTileset(), new Point(1, 11));
+    }
+
+    @Override
+    public ArrayList<Enemy> loadEnemies() {
+        ArrayList<Enemy> enemies = new ArrayList<>();
+        enemies.add(new BugEnemy(getPositionByTileIndex(8, 22), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(9, 22), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(10, 22), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(11, 22), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(12, 22), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(13, 22), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(14, 22), Direction.LEFT));
+        
+        
+        enemies.add(new BugEnemy(getPositionByTileIndex(27, 21), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(29, 21), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(31, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(33, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(35, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(37, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(39, 21), Direction.LEFT));
+        
+        enemies.add(new BugEnemy(getPositionByTileIndex(49, 21), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(50, 21), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(51, 21), Direction.RIGHT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(52, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(53, 21), Direction.LEFT));
+        enemies.add(new BugEnemy(getPositionByTileIndex(54, 21), Direction.LEFT));
+        
+        enemies.add(new DinosaurEnemy(getPositionByTileIndex(25, 11).addY(2), getPositionByTileIndex(22, 1).addY(2), Direction.RIGHT));
+        return enemies;
+    }
+
+    @Override
+    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+        enhancedMapTiles.add(new HorizontalMovingPlatform(
+                ImageLoader.load("GreenPlatform.png"),
+                getPositionByTileIndex(8, 20),
+                getPositionByTileIndex(14, 20),
+                TileType.JUMP_THROUGH_PLATFORM,
+                3,
+                new Rectangle(0, 6,16,4),
+                Direction.RIGHT
+        ));
+
+        enhancedMapTiles.add(new EndLevelBox(
+                getPositionByTileIndex(51, 10)
+        ));
+
+        return enhancedMapTiles;
+    }
+
+    
+    }
